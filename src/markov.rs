b7ff1918ea8b0eb71d92e1df205fd0b64f381fe6
Douglas Luman <dluman@douglasjluman.com>
2020-04-11 14:39:59 +0000

    And other times I document my code

--- src/markov.rs
+++ src/markov.rs
@@ -9,36 +9,46 @@ use std::io::Read;
 
 fn is_upper(c: &String)
   -> bool {
+    
     let mut upper: bool = false;
+    
+    // Prevent a general panic on none value
     if c.is_empty() {
       return upper;
     }
+    
+    // Test if the first character is uppercase
     upper = c
       .chars()
       .next()
       .unwrap()
       .is_ascii_uppercase();
+      
     upper
   }
 
 fn weighted_char(map: HashMap<String, usize>)
   -> String {
     
+    // Prevent a general panic on none value
     if map.is_empty() {
       return " ".to_string();
     }
   
     let mut choice = String::new();
     
+    // Set up sum for probability distribution
     let mut sum = 0;
     
     for (_,value) in &map {
       sum += value;
     }
     
+    // Generate a random seed to choose from probability distribution
     let mut seed = rand::thread_rng()
       .gen_range(0,sum);
-      
+    
+    // Choose from available probabilities
     'gen: for (key,value) in &map {
       if seed <= *value {
           choice = key.to_string();
@@ -63,15 +73,18 @@ fn generate_text(length: usize, table: HashMap<String,HashMap<String, usize>>, o
     // Get random first character from keys
     let mut first_char = String::new();
     
+    // Make sure that the first character is uppercase
     while !is_upper(&first_char) {
-    first_char = keys
-       .choose(&mut rand::thread_rng())
-       .unwrap()
-       .to_string();
+      first_char = keys
+        .choose(&mut rand::thread_rng())
+        .unwrap()
+        .to_string();
     }
     
+    // Add the first character to the resultant String
     result.push_str(&first_char);
     
+    // Set up subsequent character generation
     let mut next_char;
     for _ in 0..(length / order) {
       match table.get(&first_char) {
@@ -82,6 +95,7 @@ fn generate_text(length: usize, table: HashMap<String,HashMap<String, usize>>, o
           .to_owned(),
       }
 
+      // Add characters to resultant string, update search character
       first_char = next_char.to_owned();
       result.push_str(&next_char);
     }
@@ -125,26 +139,39 @@ fn generate_table(contents: &String, order: usize)
 
 fn read(filename: String)
   -> String {
+  
+    // Read the file
     let mut contents = String::new();
+  
     let mut fh = File::open(filename)
       .expect("Unable to open file.");
+    
     fh.read_to_string(&mut contents)
       .expect("Could not read file.");
+    
     contents
   }
 
 fn main() {
+
+  // Do all sorts of jank argparsing and function calling
   let args: Vec<String> = env::args()
     .skip(1)
     .collect();
+  
   let contents = read(args[0].clone());
+  
   let order = args[1]
     .parse::<usize>()
     .unwrap();
+  
   let length = args[2]
     .parse::<usize>()
     .unwrap();
+  
   let table = generate_table(&contents, order);
+  
   let result = generate_text(length, table, order);
+  
   println!("{}",result);
 }
